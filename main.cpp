bb9a2cfa671a91a7ed485f253541157deabbd06c
yuru-c <shannon0137@gmail.com>
2026-05-18 15:43:41 +0800

    Chapter 2: Successfully generated the first PPM image

--- main.cpp
+++ main.cpp
@@ -6,10 +6,12 @@ int main() {
     //PPM格式的標頭 P3代表純文字的彩色圖像格式 255代表色彩的最大值(0-255)
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
     for (int j = 0; j < image_height; j++) {
+        // \r 讓游標回到行首，std::flush 讓文字立刻顯示在終端機上
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for ( int i = 0; i < image_width; i++) {
             //正規化
             auto r = double(i) / (image_width-1); //左右 紅光
-            auto g = double(j) / (image_height-1); //上下 藍光
+            auto g = double(j) / (image_height-1); //上下 綠光
             auto b = 0.0; //藍固定0
             int ir = static_cast<int>(255.999 * r);
             int ig = static_cast<int>(255.999 * g);
@@ -17,5 +19,5 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
-
+    std::clog << "\rDone.                 \n";
 }
